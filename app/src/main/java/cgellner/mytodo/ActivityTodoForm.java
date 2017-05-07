5d28a43bd599bac4a0d07f73d61f1a232919569c
Carolin <raketensofa@hotmail.com>
2017-05-07 16:43:54 +0200

    1.0.0.3 | 07.05.2017 | 16:43

--- app/src/main/java/cgellner/mytodo/ActivityTodoForm.java
+++ app/src/main/java/cgellner/mytodo/ActivityTodoForm.java
@@ -1,15 +1,112 @@
 package cgellner.mytodo;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Locale;
 
 public class ActivityTodoForm extends Activity {
 
+    private Button buttonSave;
+    private EditText name;
+    private EditText description;
+    private TextView dateEditText;
+    private TextView time;
+    private Switch isFavourite;
+
+    private DatePicker datePicker;
+    private TimePicker timePicker;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_activity_todo_form);
 
+        //name = (EditText)findViewById(R.id.todo_name_edittext);
+        //description = (EditText)findViewById(R.id.todo_description_edittext);
+        //dateEditText = (EditText)findViewById(R.id.todo_date_edittext);
+        //isFavourite = (Switch)findViewById(R.id.todo_isfavourite_switch);
 
+        setListener();
+    }
+
+
+
+    private void setListener(){
+
+        dateEditText = (EditText)findViewById(R.id.todo_date_edittext);
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Calendar myCalendar = Calendar.getInstance();
+                final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                        myCalendar.set(Calendar.YEAR, year);
+                        myCalendar.set(Calendar.MONTH, monthOfYear);
+                        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        String myFormat = "dd.MM.yyyy";
+                        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.getDefault());
+
+                        dateEditText.setText(sdf.format(myCalendar.getTime()));
+                    }
+
+                };
+                new DatePickerDialog(v.getContext(), date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+            }
+        });
+
+
+        time = (EditText)findViewById(R.id.todo_time_edittext);
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Calendar myCalendar = Calendar.getInstance();
+                final TimePickerDialog.OnTimeSetListener timePickerListener =
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            public void onTimeSet(TimePicker view, int selectedHour,
+                                                  int selectedMinute) {
+
+                                myCalendar.set(Calendar.HOUR, selectedHour);
+                                myCalendar.set(Calendar.MINUTE, selectedMinute);
+
+                                String myFormat = "HH:mm";
+                                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.getDefault());
+                                time.setText(sdf.format(myCalendar.getTime()));
+                            }
+                        };
+
+                new TimePickerDialog(v.getContext(), timePickerListener, myCalendar.get(Calendar.HOUR), myCalendar.get(Calendar.MINUTE), true).show();
+
+            }
+        });
+/**
+        buttonSave = (Button)findViewById(R.id.button_newTodo_save);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
     }
 }
